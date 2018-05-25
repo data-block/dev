cb5fb60914def323db90ebbb31b32769fa7470d4
Andrew Jang <datablocklab@gmail.com>
2018-05-25 23:21:29 +0900

    SQL Data Summary Query

--- SQL/data_summary_script.sql
+++ SQL/data_summary_script.sql
@@ -1,3 +1,9 @@
+/*-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+											데이터 추출
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --*/
+
 /*
 - 2017년 7월까지 계정별 집행원가를 다음과 같은 조건으로 조회되도록 작성하기.
   - 계정ID, 계정명, 전년누계, 전월누계, 당월, 당월누계, 총계
@@ -8,53 +14,23 @@
     - 총계 : 전년누계 + 당월누계
  */
 
+select
+   a.ACCT_ID as 계정ID
+  ,a.ACCT_NM as 계정명
 --  전년 누계 (~2016-12)
-select
-  a.ACCT_ID as 계정ID,
-  a.ACCT_NM as 계정명,
-  sum(cost) as 총계
-from TED_ACCOUNT a, TED_COST b
-where a.ACCT_ID = b.ACCT_ID
-  and b.YEAR_MON <= '2016/12'
-group by a.ACCT_ID, a.ACCT_NM
-union
---  전월 누계 (2017-01~2017-06)
-select
-  a.ACCT_ID as 계정ID,
-  a.ACCT_NM as 계정명,
-  sum(cost) as 총계
-from TED_ACCOUNT a, TED_COST b
-where a.ACCT_ID = b.ACCT_ID
-  and b.YEAR_MON between '2017/01' and '2017/06'
-group by a.ACCT_ID, a.ACCT_NM
-union
--- 당월 누계 (전월 + 당월)
-select
-  a.ACCT_ID as 계정ID,
-  a.ACCT_NM as 계정명,
-  sum(cost) as 총계
-from TED_ACCOUNT a, TED_COST b
-where a.ACCT_ID = b.ACCT_ID
-  and b.YEAR_MON >= '2017/01'
-group by a.ACCT_ID, a.ACCT_NM
-union
--- 당월
-select
-  a.ACCT_ID as 계정ID,
-  a.ACCT_NM as 계정명,
-  sum(cost) as 총계
-from TED_ACCOUNT a, TED_COST b
-where a.ACCT_ID = b.ACCT_ID
-  and b.YEAR_MON = '2017/07'
-group by a.ACCT_ID, a.ACCT_NM
-union
---  총계 (전년누계 + 당월누계)
-select
-  a.ACCT_ID as 계정ID,
-  a.ACCT_NM as 계정명,
-  sum(cost) as 총계
-from TED_ACCOUNT a, TED_COST b
-where a.ACCT_ID = b.ACCT_ID
+,sum(case when b.year_mon <= substr(:year_mon,1,4) then cost end) as 전년누계
+--   전월 누계 (2017-01~2017-06)
+,sum(case when replace(b.YEAR_MON, '/', '') between replace(:year_mon, '/', '') - 6 and replace(:year_mon, '/', '') - 1 then cost end) as 전월누계
+--  당월
+,sum(case when b.YEAR_MON like :year_mon || '%' then cost end) as 당월
+--   당월 누계 (전월 + 당월)
+,sum(case when b.year_mon like substr(:year_mon,1,4) || '%'  then cost end) as 당월누계
+--   총계 (전년누계 + 당월누계)
+,sum(cost) as 총계
+from
+TED_ACCOUNT a, TED_COST b
+where
+a.ACCT_ID = b.ACCT_ID
 group by a.ACCT_ID, a.ACCT_NM
 order by 1, 2
 ;
